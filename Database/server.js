0013611d05c1390581d5c614881fdb4084d9cc53
Mateo Santucci <110945285+MateSantucci@users.noreply.github.com>
2023-02-21 17:40:32 -0300

    Add files via upload

---
+++ Database/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const CORS = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = 8080;
+    this.productsPath = "/products";
+  }
+
+  middlewares() {
+    this.app.use(CORS());
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use(this.productsPath, require("../routes/products.route"));
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Server running on port", this.port);
+    });
+  }
+
+  start() {
+    this.middlewares(); 
+    this.routes(); 
+    this.listen();
+  }
+}
+
+module.exports = Server;
---
+++ class/ProductManager.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+class ProductManager {
+  constructor(filePath) {
+    this.path = filePath;
+  }
+
+  // Private method: Read file and return content.
+  async #readFile() {
+    try {
+      const content = await fs.promises.readFile(this.path, 'utf-8');
+      return JSON.parse(content);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getProducts() {
+    const fileContent = await this.#readFile();
+
+    try {
+      if (fileContent.length === 0) throw new Error(`Error: No hay productos.`);
+      else return fileContent;
+    } catch (error) {
+      console.log(`Error: No hay productos.`);
+    }
+  }
+
+  // Public methods: Get a product by id.
+  async getProductById(id) {
+    try {
+      const fileContent = await this.#readFile();
+
+      if (!fileContent.find((obj) => obj.id === id)) throw new Error(`Error: No existen productos con ese ID ${id}.`);
+      else return fileContent.find((obj) => obj.id === id);
+    } catch (error) {
+      console.log(`Error: No existen productos con ese ID ${id}.`);
+    }
+  }
+}
+
+module.exports = ProductManager;
