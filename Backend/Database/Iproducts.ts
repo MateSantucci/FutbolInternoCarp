dc53eb60e93af546b4ddc9e25ed0b85291962cb5
Mateo Santucci <110945285+MateSantucci@users.noreply.github.com>
2023-02-21 17:37:49 -0300

    Delete Backend/Database directory

--- Backend/Database/Iproducts.ts
+++
@@ -1,6 +0,0 @@
-import { IProduct } from "../interface/product";
-
-const entrenamiento: IProduct = { title: "Dias y horarios", description: "Funcional", price: 3000, code: 1, stock: 10 };
-const genero: IProduct = { title: "Femenino y masculino", description: "Clases separadas", price: 4000, code: 2, stock: 20 };
-
-export { entrenamiento, genero }
